65f321851a4bda92b1c7d07930cd686f9ecc234f
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-25 11:52:50 +0530

    Code Commit

---
+++ Example2/Sol_Demo/Sol_Demo/Sol_Demo/Components/MasterPageLayoutDemo.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sol_Demo.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPageLayoutDemo : ContentView
+    {
+        public MasterPageLayoutDemo()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty MasterLayoutContentProperty =
+        BindableProperty.Create(nameof(MasterLayoutContent), typeof(View), typeof(MasterPageLayoutDemo));
+
+        public View MasterLayoutContent
+        {
+            get { return (View)GetValue(MasterLayoutContentProperty); }
+            set { SetValue(MasterLayoutContentProperty, value); }
+        }
+    }
+}
